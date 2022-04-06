e556572c75ef618ba6a19791126215d2f3bd0aa9
Emine yavuz <emineyavuz@Emines-MBP.lan>
2022-04-06 10:29:03 -0400

    added file

---
+++ maven_example/src/main/java/com/example/maven_example/MavenExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.maven_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenExampleApplication.class, args);
+	}
+
+}
